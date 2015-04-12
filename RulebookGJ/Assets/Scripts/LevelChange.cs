33f64430e54bed37c4ada1a197d0dab427da70eb
jwardlaw <jwardlaw@uci.edu>
2015-04-12 11:23:57 -0700

    Edited Prefabs, added Menu scene and LevelChange script

--- RulebookGJ/Assets/Scripts/LevelChange.cs
+++ RulebookGJ/Assets/Scripts/LevelChange.cs
@@ -3,8 +3,27 @@ using System.Collections;
 
 public class LevelChange : MonoBehaviour 
 {
-	void AdvanceLevel()
+
+	public AudioSource audio;
+
+	private bool playexitsoundonce = true;
+
+	void Awake()
 	{
+		audio = GetComponent<AudioSource> ();
+	}
+	IEnumerator AdvanceLevel()
+	{
+		if (playexitsoundonce) 
+		{
+			audio.Play ();
+			playexitsoundonce = false;
+		}
+
+		yield return new WaitForSeconds (0.5f);
+
+		float fade = GameObject.Find ("Player").GetComponent<SceneFading> ().BeginFade (1);
+		yield return new WaitForSeconds (fade);
 		Application.LoadLevel (Application.loadedLevel + 1);
 	}
 
@@ -12,7 +31,7 @@ public class LevelChange : MonoBehaviour
 	{
 		if (coll.name == "Player") 
 		{
-			AdvanceLevel ();
+			StartCoroutine(AdvanceLevel ());
 		}
 	}
 }
